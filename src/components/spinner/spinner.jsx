08b3cfe2f94ccfec1fe42d3b7721e3cf6346d841
Lali Gulbani <lali_hulbani@epam.com>
2019-11-19 19:42:45 +0300

    Add spinner

---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,13 @@
+
+const Spinner = () => {
+  return (
+    <div className="lds-css">
+      <div className="lds-double-ring">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
